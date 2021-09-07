479b4e671e97f01d5e1062b986736f981a21733c
eggman89 <official.snehasis@gmail.com>
2021-09-06 19:37:54 -0700

    Add req.txt

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import flask
+import requests
+import pandas as pd
+
 print ("Hello")
 
 print ("Staging")
+
+x = 2 +3
+print(x)
+
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data=d)
+print (df)
